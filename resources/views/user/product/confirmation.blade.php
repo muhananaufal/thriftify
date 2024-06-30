9d42a22ed52b612a6399e8737fe1b1d6fb102fb0
muhananaufal <muhananaufal8@gmail.com>
2024-07-01 00:29:33 +0700

    innitial commit

--- resources/views/user/product/confirmation.blade.php
+++ resources/views/user/product/confirmation.blade.php
@@ -116,6 +116,19 @@
                                                                 </circle>
                                                             </svg>
                                                             <p>Order Details</p>
+                                                        </a>                                                        
+                                                        <a href="{{ route('confirmation.reject', $order->id) }}" onclick="event.preventDefault(); document.getElementById('pay').submit();"
+
+                                                        class="flex gap-2 items-center px-4 py-2 text-sm hover:bg-red-100 text-red-500" role="menuitem"
+                                                            tabindex="-1" id="user-menu-item-0">
+                                                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
+                                                            <form id="pay" action="{{ route('confirmation.reject', $order->id) }}"
+                                                                method="POST" class="hidden">
+                                                                @csrf
+                                                                @method('put')
+                                                            </form>
+
+                                                            <p>Cancel</p>
                                                         </a>
 
                                                     </div>

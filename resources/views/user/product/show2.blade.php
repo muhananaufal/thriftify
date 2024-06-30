d6029a0266c2d0a61f778d27e81d46ddecb57cad
muhananaufal <muhananaufal8@gmail.com>
2024-06-30 19:56:14 +0700

    product details on landing page

--- resources/views/user/product/show2.blade.php
+++ resources/views/user/product/show2.blade.php
@@ -23,16 +23,20 @@
           <div class="flex items-center mt-2 mb-4">
             <span class="-600 text-xl font-semibold">${{ $product->price }}</span>
           </div>
-          <div class="flex items-center space-x-4 py-2">
-            <form action="{{ route('cart.add', $product->id) }}" method="POST">
-                @csrf
-            <button type="submit" href="edit-profile.html"
-                class="text-blue-500 transition-all hover:bg-blue-100 px-4 py-2 rounded-md flex gap-2 items-center">
-                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="10"></circle><line x1="12" y1="8" x2="12" y2="16"></line><line x1="8" y1="12" x2="16" y2="12"></line></svg> Add to Cart</button>
-
-
-            </form>
-
+          <p class="text-gray-600 mb-4 text-xs md:text-sm">
+            {{ $product->description }}
+          </p>
+          <div class="flex items-center space-x-4 mt-12">
+            <button
+              class="bg-dark text-white px-6 py-2 border hover:border-dark hover:bg-gray-50 hover:text-dark rounded-md mb-4 text-sm w-full"
+            >
+              Add to Cart
+            </button>
+            <button
+              class="bg-cream text-dark px-6 py-2 border hover:border-dark hover:bg-gray-50 rounded-md mb-4 text-sm w-full"
+            >
+              Buy Now
+            </button>
           </div>
           <div class="flex items-center space-x-4 text-xs pt-1 md:text-s,">
             <span>Categories: </span>
@@ -70,8 +74,14 @@
             >
           </li> -->
         </ul>
-        <div id="description" class="py-4 text-xs md:text-sm min-h-40">
-          <p>{{ $product->description }}
+        <div id="description" class="py-4 text-xs md:text-sm">
+          <p>
+            Kaos dengan gaya thrift yang menghadirkan nuansa retro dan vintage
+            yang unik. Desain grafis yang mencerminkan era 80-an dengan
+            kombinasi warna yang kontras dan mencolok. Bahan katun ringan yang
+            memberikan kenyamanan maksimal saat dipakai sehari-hari. Cocok untuk
+            mereka yang menyukai gaya unik dan ingin tampil beda dengan pakaian
+            yang menceritakan cerita masa lalu.
           </p>
         </div>
         <!-- <div id="reviews" class="py-4 hidden text-xs md:text-sm">
@@ -81,7 +91,7 @@
             Bagus untuk dipakai casual maupun santai.
           </p>
         </div> -->
-        {{-- <div class="border-t border-gray-300 my-6 mx-24" disabled></div> --}}
+        <div class="border-t border-gray-300 my-6 mx-24" disabled></div>
 
         <!-- Store Information -->
 
@@ -90,24 +100,25 @@
           <div class="flex gap-5 item-center my-5">
             <div class="drop-shadow-sm">
               <img
-                src="{{ $product->store->picture }}"
+                src="../asset/img/Bucket-Hat.jpeg"
                 alt=""
                 srcset=""
                 class="h-20 w-20 aspect-square object-cover rounded-full"
               />
             </div>
             <div>
-              <p class="text-gray-600 text-xs md:text-lg">
-                <strong>Store Name:</strong> {{ $product->store->name }}
+              <p class="text-gray-600 text-sm md:text-lg">
+                <strong>Store Name:</strong> Awesome Shoe Store
               </p>
-              <p class="text-gray-600 text-xs md:text-lg">
-                <strong>Location:</strong> {{ $product->store->location }}
+              <p class="text-gray-600 text-sm md:text-lg">
+                <strong>Location:</strong> 123 Shoe Street, Footwear City,
+                Country
               </p>
-              {{-- <p class="text-gray-600 text-xs md:text-lg">
+              <p class="text-gray-600 text-sm md:text-lg">
                 <strong>Contact:</strong> (123) 456-7890
-              </p> --}}
-              <p class="text-gray-600 text-xs md:text-lg">
-                <strong>Email:</strong> {{ $product->store->email }}
+              </p>
+              <p class="text-gray-600 text-sm md:text-lg">
+                <strong>Email:</strong> info@shoesstore.com
               </p>
             </div>
           </div>
@@ -119,7 +130,7 @@
     <div class="border-t border-gray-300 my-6 mx-24" disabled></div>
 
     <!-- Section: Produk Terbaru -->
-    {{-- <section class="container mx-auto mt-10 mb-8 px-4 md:px-8">
+    <section class="container mx-auto mt-10 mb-8 px-4 md:px-8">
       <!-- Produk Terbaru Content -->
       <div class="mb-4">
         <h2 class="text-gray-800 font-bold text-xl mb-16 flex justify-center">
@@ -186,6 +197,72 @@
           </div>
         </div>
       </div>
-    </section> --}}
-    {{-- <div class="border-t border-gray-300 my-8 mx-24" disabled></div> --}}
+    </section>
+    <div class="border-t border-gray-300 my-8 mx-24" disabled></div>
+
+    <footer class="p-8 mx-auto bg-gray-100 text-sm">
+      <div class="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-4 md:gap-8">
+        <div class="mb-8 md:mb-0">
+          <h4 class="font-bold mb-4">Thriftify</h4>
+          <ul>
+            <li>
+              <p class="text-sm">Thriftify is a platform for thrifting clothes and accessories. We provide a platform for sellers to sell their preloved items and buyers to buy them. We are committed to providing the best service for our users.</p>
+            </li>
+          </ul>
+        </div>
+        <div class="mb-8 md:mb-0">
+          <h4 class="font-bold mb-4">Buy</h4>
+          <ul>
+            <li><a href="#" class="hover:underline">Men Clothes</a></li>
+            <li><a href="#" class="hover:underline">Women Clothes</a></li>
+            <li><a href="#" class="hover:underline">Sportwear</a></li>
+          </ul>
+          <h4 class="font-bold mt-8 mb-4">Sell</h4>
+          <ul>
+            <li>
+              <a href="#" class="hover:underline">Seller Education Center</a>
+            </li>
+            <li>
+              <a href="#" class="hover:underline">Official Store Registration</a>
+            </li>
+          </ul>
+        </div>
+        <div class="mb-8 md:mb-0">
+          <h4 class="font-bold mb-4">Help</h4>
+          <ul>
+            <li><a href="#" class="hover:underline">Contact Us</a></li>
+            <li>
+              <a href="#" class="hover:underline">Terms and Conditions</a>
+            </li>
+            <li><a href="#" class="hover:underline">Privacy Policy</a></li>
+          </ul>
+        </div>
+        <div>
+          <h4 class="font-bold mb-4">Follow Us</h4>
+          <div class="flex space-x-2">
+            <a href="#" class="hover:underline">
+              <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="#000"><path d="M12 0c6.627 0 12 5.373 12 12s-5.373 12-12 12S0 18.627 0 12 5.373 0 12 0zm4 7.278V4.5h-2.286c-2.1 0-3.428 1.6-3.428 3.889v1.667H8v2.777h2.286V19.5h2.857v-6.667h2.286L16 10.056h-2.857V8.944c0-1.11.572-1.666 1.714-1.666H16z"/></svg>
+            </a>
+            <a href="#" class="hover:underline">
+              <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="#000"><path d="M12 0c6.627 0 12 5.373 12 12s-5.373 12-12 12S0 18.627 0 12 5.373 0 12 0zm3.193 7c-1.586 0-2.872 1.243-2.872 2.777 0 .217.025.43.074.633a8.251 8.251 0 0 1-5.92-2.902c-.247.41-.389.887-.389 1.397 0 .963.507 1.813 1.278 2.311a2.94 2.94 0 0 1-1.301-.348v.036c0 1.345.99 2.467 2.304 2.723a2.98 2.98 0 0 1-1.298.047c.366 1.103 1.427 1.906 2.683 1.928a5.889 5.889 0 0 1-3.567 1.19c-.231 0-.46-.014-.685-.04A8.332 8.332 0 0 0 9.903 18c5.283 0 8.172-4.231 8.172-7.901 0-.12-.002-.24-.008-.36A5.714 5.714 0 0 0 19.5 8.302a5.869 5.869 0 0 1-1.65.437 2.8 2.8 0 0 0 1.263-1.536 5.87 5.87 0 0 1-1.824.674A2.915 2.915 0 0 0 15.193 7z"/></svg>
+            </a>
+            <a href="#" class="hover:underline">
+              <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="#000"><path d="M12 0c6.6274 0 12 5.3726 12 12s-5.3726 12-12 12S0 18.6274 0 12 5.3726 0 12 0zm3.115 4.5h-6.23c-2.5536 0-4.281 1.6524-4.3805 4.1552L4.5 8.8851v6.1996c0 1.3004.4234 2.4193 1.2702 3.2359.7582.73 1.751 1.1212 2.8818 1.1734l.2633.006h6.1694c1.3004 0 2.389-.4234 3.1754-1.1794.762-.734 1.1817-1.7576 1.2343-2.948l.0056-.2577V8.8851c0-1.2702-.4234-2.3589-1.2097-3.1452-.7338-.762-1.7575-1.1817-2.9234-1.2343l-.252-.0056zM8.9152 5.8911h6.2299c.9072 0 1.6633.2722 2.2076.8166.4713.499.7647 1.1758.8103 1.9607l.0063.2167v6.2298c0 .9375-.3327 1.6936-.877 2.2077-.499.4713-1.176.7392-1.984.7806l-.2237.0057H8.9153c-.9072 0-1.6633-.2722-2.2076-.7863-.499-.499-.7693-1.1759-.8109-2.0073l-.0057-.2306V8.885c0-.9073.2722-1.6633.8166-2.2077.4712-.4713 1.1712-.7392 1.9834-.7806l.2242-.0057h6.2299-6.2299zM12 8.0988c-2.117 0-3.871 1.7238-3.871 3.871A3.8591 3.8591 0 0 0 12 15.8408c2.1472 0 3.871-1.7541 3.871-3.871 0-2.117-1.754-3.871-3.871-3.871zm0 1.3911c1.3609 0 2.4798 1.119 2.4798 2.4799 0 1.3608-1.119 2.4798-2.4798 2.4798-1.3609 0-2.4798-1.119-2.4798-2.4798 0-1.361 1.119-2.4799 2.4798-2.4799zm4.0222-2.3589a.877.877 0 1 0 0 1.754.877.877 0 0 0 0-1.754z"/></svg>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div class="flex justify-between items-center mt-8">
+        <div class="mb-4 md:mb-0">
+          <span class="text-gray-700">&copy; <script>document.write(new Date().getFullYear())</script> Thriftify. All rights reserved.</span>
+        </div>
+      </div>
+    </footer>
+
+    <!-- JavaScript -->
+    <script src="../src/js/index.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.js"></script>
+  </body>
+</html>
+
 @endsection
